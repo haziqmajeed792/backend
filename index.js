567e270181cfbfd362d86014b18506cfdb225c69
haziqmajeed792 <132041773+haziqmajeed792@users.noreply.github.com>
2025-03-03 13:52:39 +0400

    update express limit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const IDENTITY_API_BASE_URL = process.env.IDENTITY_API_BASE_URL;
 const API_KEY = process.env.API_KEY;
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
